d91f7f8de80e2d6b220015a5afb383c89572a6c8
Bhavdeep Dhanjal <dhanjalbhavdeep@gmail.com>
2018-06-22 00:30:35 +0530

    createElement works for text elements also

--- src/element.js
+++ src/element.js
@@ -1,8 +1,19 @@
 function createElement(type, config, ...children) {
   const props = Object.assign({}, config)
-  if(children.length !== 0) props.children = children
+  const hasChildren = children.length > 0
+  if(hasChildren) {
+    const rawChildren =  [].concat(...children)
+    props.children = rawChildren
+      .filter(child => child !== null && child !== false)
+      .map(child => child instanceof Object ? child : createTextElement(child))
+  }
+  
   const element = Object.assign({}, { type, props })
   return element
+}
+
+function createTextElement(value) {
+  return createElement('TEXT_ELEMENT', { nodeValue: value })
 }
 
 module.exports = createElement
--- tests/render-jsx.test.js
+++ tests/render-jsx.test.js
@@ -10,9 +10,9 @@ describe('check that createElement creates an element for renderer', () => {
     assert.deepEqual(element, { type: 'div', props: {} })
   })
 
-  it('creates a div element with id', () => {
-    const element = Freact.createElement('div', { id: 'wrapper' })
-    assert.deepEqual(element, { type: 'div', props: { id: 'wrapper' } })
+  it('creates a div element with props', () => {
+    const element = Freact.createElement('div', { id: 'wrapper', class: 'container' })
+    assert.deepEqual(element, { type: 'div', props: { id: 'wrapper', class: 'container' } })
   })
 
   it('creates a div element with a single child', () => {
@@ -29,7 +29,7 @@ describe('check that createElement creates an element for renderer', () => {
       },
     })
   })
-  
+
   it('creates a div element with multiple children', () => {
     const element = Freact.createElement(
       'div',
@@ -43,7 +43,25 @@ describe('check that createElement creates an element for renderer', () => {
         id: 'wrapper',
         children: [
           { type: 'span', props: {} },
-          { type: 'p' , props: { children: [ { type: 'h1', props: {} } ]}}
+          { type: 'p', props: { children: [{ type: 'h1', props: {} }] } },
+        ],
+      },
+    })
+  })
+
+  it('creates a div with a text child and a span', () => {
+    const element = Freact.createElement(
+      'div',
+      null,
+      'Hola!',
+      Freact.createElement('span')
+    )
+    assert.deepEqual(element, {
+      type: 'div',
+      props: {
+        children: [
+          { type: 'TEXT_ELEMENT', props: { nodeValue: 'Hola!' } },
+          { type: 'span', props: {} }
         ]
       }
     })

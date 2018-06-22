e391249f4cc2659e2b537c6fd57327cff982e15d
Bhavdeep Dhanjal <dhanjalbhavdeep@gmail.com>
2018-06-22 17:11:27 +0530

    define brute render, reconcile and instantiate functions

---
+++ src/reconciler.js
@@ -0,0 +1,55 @@
+let rootInstance = null
+
+function render(element, container) {
+  const prevInstance = rootInstance
+  const nextInstance = reconcile(container, prevInstance, element)
+  rootInstance = nextInstance
+}
+
+function reconcile(parentDom, instance, element) {
+  if(instance == null) {
+    const newInstance = instantiate(element)
+    parentDom.appendChild(newInstance.dom)
+    return newInstance
+  } else {
+    const newInstance = instantiate(element)
+    parentDom.replaceChild(newInstance.dom, instance.dom)
+    return newInstance
+  }
+}
+
+function instantiate(element) {
+  const { type, props } = element
+
+  // Create DOM element
+  const isTextElement = type === 'TEXT_ELEMENT'
+  const dom = isTextElement
+    ? document.createTextNode('')
+    : document.createElement(type)
+
+  // add event listeners to the element
+  const isListener = name => name.startsWith('on')
+  Object.keys(props)
+    .filter(isListener)
+    .forEach(name => {
+      const eventType = name.toLowerCase().substring(2)
+      dom.addEventListener(eventType, props[name])
+    })
+
+  // add attributes to the element
+  const isAttribute = name => !isListener(name) && name !== 'children'
+  Object.keys(props)
+    .filter(isAttribute)
+    .forEach(name => {
+      dom[name] = props[name]
+    })
+
+  // instantiate and append children
+  const childElements = props.children || []
+  const childInstances = childElements.map(instantiate)
+  const childDoms = childInstances.map(childInstance => childInstance.dom)
+  childDoms.forEach(childDom => dom.appendChild(childDom))
+
+  const instance = { dom, element, childInstances }
+  return instance
+}

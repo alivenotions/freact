cb08afec3a3b653381ec5b8da0ab321bf310ccc3
Bhavdeep Dhanjal <dhanjalbhavdeep@gmail.com>
2018-06-27 20:12:58 +0530

    tests for reconciliation algorithm

---
+++ tests/reconciliation.test.js
@@ -0,0 +1,37 @@
+const assert = require('chai').assert
+
+require('./mock-browser')
+const { createElement, render } = require('../src/freact')
+/** @jsx createElement */
+
+describe('check the reconciler works correctly', () => {
+  beforeEach(function() {
+    let root = document.getElementById('root')
+    if (!root) {
+      root = document.createElement('div')
+      root.id = 'root'
+      document.body.appendChild(root)
+    }
+    this.root = root
+  })
+
+  it('replace div to span', function() {
+    const root = this.root
+    let element = <div>Tadow!</div>
+    render(element, root)
+    assert.equal(root.innerHTML, '<div>Tadow!</div>')
+    const prevChild = root.firstElementChild
+    element = <span>Tadow!</span>
+    render(element, root)
+    assert.equal(root.innerHTML, '<span>Tadow!</span>')
+    const nextChild = root.firstElementChild
+    assert.notEqual(prevChild, nextChild)
+  })
+
+  it('reuses div', function() {
+    const root = this.root
+    let element = <div>Tadow!</div>
+    render(element, root)
+    assert.equal(root.innerHTML, '<div>Tadow!</div>')
+  })
+})
